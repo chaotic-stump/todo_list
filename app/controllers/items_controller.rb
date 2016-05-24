409df3d71a03b54511913cd3263fc4d919be9e45
Shaws <seanrichardparker@gmail.com>
2016-05-24 17:17:54 -0600

    new item and show item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,34 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @items = Item.new
   end
 
   def edit
+    @items = Item.find(params[:id])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
+
+  def create
+    @items = Item.new(item_params)
+
+    if @items.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :boolean)
+  end
+
 end
